04755a4fdf09b976ab17d7872c64ad656f6aa36f
向日葵blink <echomaps@163.com>
2019-10-31 09:55:01 +0800

    test: xx

--- pages/blogs/_blog.vue
+++ pages/blogs/_blog.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <p class="main-con">这是内容{{$route.params.blog}} dev1.0 增加7</p>
+    <p class="main-con">这是内容{{$route.params.blog}} dev1.0 增加71</p>
   </div>
 </template>
 
